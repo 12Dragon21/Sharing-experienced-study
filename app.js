693b11e8f62690c320b319e095b9c78665a6dbdc
truongducthien2k2 <truongducthien2k2@gmail.com>
2023-12-12 10:47:09 +0700

    1 so page

--- app.js
+++ app.js
@@ -1,25 +1,45 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const bodyParser = require('body-parser');
 
 // Sử dụng middleware để đọc dữ liệu từ form
 app.use(express.urlencoded({ extended: true }));
+app.use('/styles', express.static(path.join(__dirname, 'styles')));
 // Đặt middleware để sử dụng thư mục views cho các tệp HTML
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
+// Đặt tuyến đường cho đường dẫn gốc
 app.get('/', (req, res) => {
-  res.render('login');
+  res.redirect('/home'); // Chuyển hướng đến trang home
 });
-app.post('/login', (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-  res.redirect('/home');
-});
+
+
+// Tuyến đường cho trang home
 app.get('/home', (req, res) => {
   res.render('home');
+});
+
+// Tuyến đường cho trang login
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+
+// Tuyến đường cho trang register
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+// Tuyến đường cho trang search
+app.get('/search', (req, res) => {
+  res.render('search');
+});
+
+// Tuyến đường cho trang viewforum
+app.get('/viewforum', (req, res) => {
+  res.render('viewforum');
 });
 const port = 3000;
 app.listen(port, () => {

cb76151cf877df0bb3142b0046858d3673b996fc
truongducthien2k2 <truongducthien2k2@gmail.com>
2023-12-13 15:19:32 +0700

    only page

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 // Sử dụng middleware để đọc dữ liệu từ form
 app.use(express.urlencoded({ extended: true }));
+// Sử dụng expressstatic để định tuyến router styles
 app.use('/styles', express.static(path.join(__dirname, 'styles')));
 // Đặt middleware để sử dụng thư mục views cho các tệp HTML
 app.set('views', path.join(__dirname, 'views'));
@@ -40,6 +41,21 @@ app.get('/search', (req, res) => {
 // Tuyến đường cho trang viewforum
 app.get('/viewforum', (req, res) => {
   res.render('viewforum');
+});
+
+// Tuyến đường cho trang viewtopic
+app.get('/viewtopic', (req, res) => {
+  res.render('viewtopic');
+});
+
+// Tuyến đường cho trang question
+app.get('/question', (req, res) => {
+  res.render('question');
+});
+
+// Tuyến đường cho trang viewpost
+app.get('/viewpost', (req, res) => {
+  res.render('viewpost');
 });
 const port = 3000;
 app.listen(port, () => {
